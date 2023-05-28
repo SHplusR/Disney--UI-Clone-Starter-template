861290bee1094d4f28c65bea3dc8c357d3f6d8f3
SHplusR <devyousim@gmail.com>
2023-05-29 00:50:21 +0900

    movie container 추가

--- app.js
+++ app.js
@@ -56,11 +56,31 @@ const videoCards = [...document.querySelectorAll('.video-card')];
 videoCards.forEach( item => {
     item.addEventListener('mouseover', () => {
         let video = item.children[1];
+        video.muted = true;
         video.play();
+        video.muted = false;
     })
 
     item.addEventListener('mouseleave', () => {
         let video = item.children[1];
         video.pause();
+    })
+})
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item,i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click',() => {
+        item.scrollLeft += containerWidth -200;
+    })
+
+    
+    preBtns[i].addEventListener('click',() => {
+        item.scrollLeft -= containerWidth +200;
     })
 })
