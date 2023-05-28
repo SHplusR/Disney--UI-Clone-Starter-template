0f8ee53ea91f646d06dd00e721ca912f85a39ce7
SHplusR <devyousim@gmail.com>
2023-05-28 23:43:59 +0900

    video card 추가

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// css선택자를 기반으로 요소를 찾는 querySelector
 let carousel = document.querySelector('.carousel');
 let sliders = [];
 
 let slideIndex = 0;
 
+// 저장된 영화의 길이보다 slideindex가 크면 다시 처음으로 돌아가기.
 const creatSlide = () =>{
     if(slideIndex >= movies.length){
         slideIndex =0;
     }
-
+//js를 통해 html요소를 생성하는 createElement
     let slide = document.createElement('div');
     let imgElement = document.createElement('img');
     let content = document.createElement('div');
@@ -17,6 +19,7 @@ const creatSlide = () =>{
     imgElement.appendChild(document.createTextNode(''));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     p.appendChild(document.createTextNode(movies[slideIndex].des));
+
     content.appendChild(h1);
     content.appendChild(p);
     slide.appendChild(content);
@@ -45,3 +48,19 @@ for(let i =0; i<6; i++){
 setInterval(() =>{
     creatSlide();
 },3000);
+
+// video card
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach( item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
